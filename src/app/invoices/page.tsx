9b9de09f777ad9f5fa74fd86bf806d5382ca43ff
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-03-06 15:15:00 +0000

    update sidebar

---
+++ src/app/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Incoices() {
+    return <>invoices</>
+}
--- src/components/ui/sidebar-content.tsx
+++ src/components/ui/sidebar-content.tsx
@@ -1,16 +1,27 @@
+'use client';
+
+import clsx from "clsx";
 import CustomHeader from "./custom-header";
+import { usePathname, useRouter } from "next/navigation";
+
+const menuItems = [{ name: 'Invoices', href: '/invoices' }, { name: 'Sponsors', href: '/sponsors' }];
 
 export default function SidebarContent() {
+
+    const pathname = usePathname();
+
+    const getClassName = (href: string) => clsx('block rounded px-4 py-2.5 transition duration-20  hover:text-white hover:bg-gray-900', {
+        'bg-gray-900 text-white': pathname === href
+    })
     return (
         <div>
-            <CustomHeader />
             <div className="mt-6">
-                {["Invoices", "Sponsors"].map((item, index) => (
+                {menuItems.map((item, index) => (
                     <a
                         key={index}
-                        href="#"
-                        className="block rounded px-4 py-2.5 transition duration-200 hover:text-white hover:bg-gray-900">
-                        {item}
+                        href={item.href}
+                        className={getClassName(item.href)}>
+                        {item.name}
                     </a>
                 ))}
             </div>
