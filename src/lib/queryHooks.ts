2225b62f745e8d9f51c882e8da69549ca630d813
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-03-29 10:26:28 +0000

    types for query custom hook

--- src/lib/queryHooks.ts
+++ src/lib/queryHooks.ts
@@ -1,10 +1,15 @@
 'use client';
 import { API_URL } from "@/constants";
-import { useQuery } from "react-query"
+import { UseQueryResult, useQuery } from "react-query"
 
-export const useReactQueryFetch = (key: string, url: string) => {
-    const URL = API_URL + url;
-    return useQuery(key, () => {
-        return fetch(URL).then(res => res.json())
-    })
+/**
+ * Fetch data from an API endpoint using React Query.
+ * @param key A unique string to identify this query
+ * @param url The URL of the endpoint to fetch data from
+ * @returns The fetched data, or `undefined` if the query is still loading
+ */
+export const useReactQueryFetch = <T>(key: string, pathname: string): UseQueryResult<T> => {
+    const URL = API_URL + pathname;
+    return useQuery<T, Error>(key, () => fetch(URL).then(res => res.json()));
 }
+
