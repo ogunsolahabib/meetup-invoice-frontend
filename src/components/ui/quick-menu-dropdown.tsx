ea4e19c2a0f19228703414203689d8af210390fb
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-03-29 10:36:08 +0000

    remove unused

--- src/components/ui/quick-menu-dropdown.tsx
+++ src/components/ui/quick-menu-dropdown.tsx
@@ -1,19 +1,6 @@
-import {
-    Cloud,
-    CreditCard,
-    Github,
-    Keyboard,
-    LifeBuoy,
-    LogOut,
-    Mail,
-    MessageSquare,
-    Plus,
-    PlusCircle,
-    Settings,
-    User,
-    UserPlus,
-    Users, Pencil
-} from "lucide-react"
+'use client';
+
+
 
 import { Button } from "@/components/ui/button"
 import {
@@ -22,24 +9,35 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import CreateSponsorModal from "../page-specific/sponsors/create-sponsor-modal"
+import { Dialog, DialogTrigger } from "./dialog"
 
 export function QuickMenuDropdown() {
     return (
-        <DropdownMenu>
+
+        <DropdownMenu modal={false}>
             <DropdownMenuTrigger asChild>
                 <Button variant='default'>Create</Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
-                <DropdownMenuItem>
-                    <Pencil className="mr-2 h-4 w-4" />
-                    <span>Invoice</span>
+            {/* <DropdownMenuContent className="w-56">
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                    <DialogTrigger>
+                        <span className="flex items-center">
+                            <Pencil className="mr-2 h-4 w-4" />
+                            <span>Invoice</span>
+                        </span>
+                        <CreateSponsorModal />
+
+                    </DialogTrigger>
                 </DropdownMenuItem>
+
                 <DropdownMenuItem>
                     <User className="mr-2 h-4 w-4" />
                     <span>Sponsor</span>
                 </DropdownMenuItem>
 
-            </DropdownMenuContent>
+            </DropdownMenuContent> */}
         </DropdownMenu>
+
     )
 }
