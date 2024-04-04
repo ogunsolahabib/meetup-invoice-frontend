abc0472f65c596ae938fd82ae5723ae3c0b95603
Habib Ogunsola <ogunsolahabib@yahoo.com>
2024-04-04 11:33:47 +0100

    update invoices page layout

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -2,5 +2,11 @@
 import InvoicesTable from "@/components/page-specific/invoices/invoices-table";
 
 export default function Incoices() {
-    return <InvoicesTable />
+
+    return <div className="container py-10">
+        <div className="container flex justify-between">
+            <h1>Invoices</h1>
+        </div>
+        <InvoicesTable />
+    </div>
 }
--- src/components/page-specific/invoices/invoices-table.tsx
+++ src/components/page-specific/invoices/invoices-table.tsx
@@ -27,29 +27,27 @@ export default function InvoicesTable(): JSX.Element {
         return <p>Loading...</p>
     }
     return (
-        <div>
-            <p>Invoices</p>
-            <DataTable
-                columns={[
-                    {
-                        accessorKey: "sponsor.name",
-                        header: "Sponsor",
+        <DataTable
+            columns={[
+                {
+                    accessorKey: "sponsor.name",
+                    header: "Sponsor",
+                },
+                {
+                    accessorKey: "total_amount",
+                    header: "Amount",
+                },
+                {
+                    accessorKey: "invoice_date",
+                    header: "Date",
+                    cell: ({ getValue }) => {
+                        return convertToDateString(getValue());
                     },
-                    {
-                        accessorKey: "total_amount",
-                        header: "Amount",
-                    },
-                    {
-                        accessorKey: "invoice_date",
-                        header: "Date",
-                        cell: ({ getValue }) => {
-                            return convertToDateString(getValue());
-                        },
-                    },
-                ]}
-                data={data ?? []}
-            />
-        </div>
+                },
+            ]}
+            data={data ?? []}
+        />
+
     )
 }
 
